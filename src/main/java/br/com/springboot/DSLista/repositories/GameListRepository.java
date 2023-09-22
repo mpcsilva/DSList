cf702717ada620bb3f4fc9a0fd166239aa6d8d4f
Marcos Paulo <mpcsilva8@gmail.com>
2023-09-22 18:31:40 -0300

    CORS CONFIG

--- src/main/java/br/com/springboot/DSLista/repositories/GameListRepository.java
+++ src/main/java/br/com/springboot/DSLista/repositories/GameListRepository.java
@@ -1,7 +1,7 @@
 package br.com.springboot.DSLista.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.springboot.DSLista.entities.Game;
+
 import br.com.springboot.DSLista.entities.GameList;
 
 /*  Responsável por realizar consultas na base de dados. */
